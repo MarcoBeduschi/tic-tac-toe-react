2f4993b3a1c623f3d72ee6d30bdee3289a2fcad4
Marco Beduschi <beduschimarco@gmail.com>
2019-08-20 08:54:08 -0300

    Render 9 Square components passing down value to each

--- src/Board.js
+++ src/Board.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import Square from './Square.js';
-import './Board.css';
 
-function Board() {
+function Board(props) {
     return (
         <div className="board">
             <div className="board-row">
-                <Square />
-                <Square />
-                <Square />
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
             </div>
             <div className="board-row">
-                <Square />
-                <Square />
-                <Square />
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
             </div>
             <div className="board-row">
-                <Square />
-                <Square />
-                <Square />
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
             </div>
         </div>
     );
+
+    function renderSquare(i) {
+        return <Square id={i} value={props.board[i]} handleSquareClick={props.handleSquareClick}/>
+    }
 }
 
 export default Board;
