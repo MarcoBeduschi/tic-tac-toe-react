b3b83ef5b7b098fecb26023c51eecb6b49e5a424
Marco Beduschi <beduschimarco@gmail.com>
2019-08-20 08:54:42 -0300

    Upgrade to React Component. Create state & pass board state down components

--- src/Game.js
+++ src/Game.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import Board from './Board.js';
 
-function Game(props) {
-    return (
-        <div className="game">
-            <div className="game-header">
-                Tic Tac Toe Game
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            turn: 1,
+            board: Array(9).fill(null)
+        };
+    }
+
+    handleSquareClick = (squareIndex) => {
+        this.setState(function(state) {
+            let newBoard = [...state.board];
+            newBoard[squareIndex] = 'X';
+
+            return {
+                board: newBoard
+            };
+        });
+    }
+    
+    render() {
+        return (
+            <div className="game">
+                <div className="game-header">
+                    <h1>Tic Tac Toe Game</h1>
+                    <h3>Turn {this.state.turn}</h3>
+                </div>
+                <Board board={this.state.board} handleSquareClick = {this.handleSquareClick}/>
             </div>
-            <Board />
-        </div>
-    );
+        );
+    }
 }
 
 export default Game;
