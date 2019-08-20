f889c8799aac3e87f91dddabe67e40ba435cf437
Marco Beduschi <beduschimarco@gmail.com>
2019-08-20 08:51:58 -0300

    :art: refactor divs on App component gst

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import Game from './Game.js';
 import './App.css';
 import './Game.css';
+import './Board.css';
+import './Square.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Game />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return(
+      <div className="app">
+        <div className="app-container">
+          <Game />
+        </div>
+      </div>
+    )
+  };
 }
 
 export default App;
