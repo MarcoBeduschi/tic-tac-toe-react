6d1a3f1449a8b3f6aa21c346e438dbbeaee393ea
Marco Beduschi <beduschimarco@gmail.com>
2019-09-17 21:16:36 -0300

    Render current player score

--- src/PlayerScore.js
+++ src/PlayerScore.js
@@ -5,16 +5,18 @@ import { faTimes, faCircle } from '@fortawesome/free-solid-svg-icons'
 function PlayerScore(props) {
   let icon
 
-  if (props.value === 'X') {
+  if (props.player === 'X') {
     icon = <FontAwesomeIcon icon={faTimes} />;
-  } else if (props.value === 'O') {
+  } else if (props.player === 'O') {
     icon = <FontAwesomeIcon icon={faCircle} />;
   }
 
   return(
     <div className="player-score-card">
       <div className="player-score-icon">{icon}</div>
-      <div className="player-score-current-score">-</div>
+      <div className="player-score-current-score">
+        {props.score ? props.score : "-"}
+      </div>
     </div>
   )
 }
