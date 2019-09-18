800b0a5bbc779bc7bd5e481d53b5dc68cfd7fa06
Marco Beduschi <beduschimarco@gmail.com>
2019-09-17 21:17:20 -0300

    :art: Indentation

--- src/PlayerScore.js
+++ src/PlayerScore.js
@@ -3,22 +3,22 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes, faCircle } from '@fortawesome/free-solid-svg-icons'
 
 function PlayerScore(props) {
-  let icon
+    let icon
 
-  if (props.player === 'X') {
-    icon = <FontAwesomeIcon icon={faTimes} />;
-  } else if (props.player === 'O') {
-    icon = <FontAwesomeIcon icon={faCircle} />;
-  }
+    if (props.player === 'X') {
+        icon = <FontAwesomeIcon icon={faTimes} />;
+    } else if (props.player === 'O') {
+        icon = <FontAwesomeIcon icon={faCircle} />;
+    }
 
-  return(
-    <div className="player-score-card">
-      <div className="player-score-icon">{icon}</div>
-      <div className="player-score-current-score">
-        {props.score ? props.score : "-"}
-      </div>
-    </div>
-  )
+    return(
+        <div className="player-score-card">
+            <div className="player-score-icon">{icon}</div>
+            <div className="player-score-current-score">
+                {props.score ? props.score : "-"}
+            </div>
+        </div>
+    )
 }
 
 export default PlayerScore;
