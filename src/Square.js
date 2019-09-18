7775a63011ae8ed3688269a4f3719666e2cd5053
Marco Beduschi <beduschimarco@gmail.com>
2019-09-17 21:34:30 -0300

    :art: Indentation

--- src/Square.js
+++ src/Square.js
@@ -3,19 +3,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes, faCircle } from '@fortawesome/free-solid-svg-icons'
 
 function Square(props) {
-  let icon
+    let icon
 
-  if (props.value === 'X') {
-    icon = <FontAwesomeIcon icon={faTimes} />;
-  } else if (props.value === 'O') {
-    icon = <FontAwesomeIcon icon={faCircle} />;
-  }
+    if (props.value === 'X') {
+        icon = <FontAwesomeIcon icon={faTimes} />;
+    } else if (props.value === 'O') {
+        icon = <FontAwesomeIcon icon={faCircle} />;
+    }
 
-  return(
-    <div className="square" onClick={props.onClick}>
-      <span className={`square-content square-content-${props.value}`}>{icon}</span>
-    </div>
-  )
+    return(
+        <div className="square" onClick={props.onClick}>
+            <span className={`square-content square-content-${props.value}`}>{icon}</span>
+        </div>
+    )
 }
 
 export default Square;
