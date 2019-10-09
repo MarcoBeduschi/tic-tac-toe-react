1a36e9f597ec2b970051cbd9166860c8f6ead6b2
Marco Beduschi <beduschimarco@gmail.com>
2019-10-09 13:22:32 -0300

    Move Game to function component w/ react hooks

--- src/Game.js
+++ src/Game.js
@@ -1,31 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Match from './Match.js'
 
-class Game extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            winnerHistory: []
-        }
+const Game = () => {
+    const [winnerHistory, setWinnerHistory] = useState([]);
+    
+    const onMatchOver = (winner) => {
+        setWinnerHistory((prevWinnerHistory) => [...prevWinnerHistory, winner]);
     }
 
-    render() {
-        return (
-            <div className="game">
-                <Match winnerHistory={this.state.winnerHistory} onMatchOver={this.onMatchOver}/>
-            </div>
-        );
-    }
-
-    onMatchOver = (winner) => {
-        this.setState((prevState) => {
-            const newWinnerHistory = [...prevState.winnerHistory, winner]
-
-            return {
-                winnerHistory: newWinnerHistory
-            }
-        })
-    }
+    return (
+        <div className="game">
+            <Match winnerHistory={winnerHistory} onMatchOver={onMatchOver}/>
+        </div>
+    );
 }
+
 
 export default Game;
